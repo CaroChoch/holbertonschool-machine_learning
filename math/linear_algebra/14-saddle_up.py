a2520af47b91bdcdaa3e3282e47f3ae94f2a1bc8
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-12-05 15:01:42 -0800

    function that performs matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    function that performs matrix multiplication
+        Argument:
+            mat1: the first matrix
+            mat2: the second matrix
+        Return:
+            a new matrix
+    """
+    return np.matmul(mat1, mat2)
