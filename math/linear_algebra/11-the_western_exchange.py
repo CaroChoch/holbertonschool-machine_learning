3912f750953b4d90bc06c4946b4edba365cd27c7
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-12-05 14:20:27 -0800

    function that transposes matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that transposes matrix"""
+
+
+def np_transpose(matrix):
+    """
+    function that transposes matrix
+        Argument:
+            matrix: the matrix to transpose
+        Return:
+            a new numpy.ndarray
+    """
+    return matrix.transpose()
