9173c7273b5345f0b68d8a4e148cb761be3f1ee3
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-12-05 04:03:25 -0800

    pycodestyle (lines too long)

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -7,13 +7,15 @@ def matrix_shape(matrix):
         Arguments:
             matrix: matrix to size
         Return:
-            a list of integers 
+            a list of integers
     """
     # Check if the first element of the matrix is a list
     if type(matrix[0]) == list:
         # If it is, return a list containing the length of the matrix
-        # and the result of the recursive call to the function on the first element of the matrix
-        return [len(matrix)] + matrix_shape(matrix[0])   
+        # and the result of the recursive call to the function on the
+        # first element of the matrix
+        return [len(matrix)] + matrix_shape(matrix[0])
     else:
-        # If the first element is not a list, simply return a list containing the length of the matrix
+        # If the first element is not a list, simply return a list containing
+        # the length of the matrix
         return [len(matrix)]
