4458ec1a8902a2c94e12efa1c720dbf7bd92e68d
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-12-05 03:58:37 -0800

    function that calculates the shape of a matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""function that calculates the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """function that calculates the shape of a matrix
+        Arguments:
+            matrix: matrix to size
+        Return:
+            a list of integers 
+    """
+    # Check if the first element of the matrix is a list
+    if type(matrix[0]) == list:
+        # If it is, return a list containing the length of the matrix
+        # and the result of the recursive call to the function on the first element of the matrix
+        return [len(matrix)] + matrix_shape(matrix[0])   
+    else:
+        # If the first element is not a list, simply return a list containing the length of the matrix
+        return [len(matrix)]
