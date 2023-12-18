999f9e7c4c5ce750e55077cfac0866a4a237ba0f
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-12-18 09:26:32 -0800

    using a loop

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -27,10 +27,16 @@ class Normal:
             # If data is given, calculate mean and stddev from the data
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
+            # If data doesn't contain at least two data points raise ValueError
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
                 # Calculate mean and stddev from the data
                 self.mean = float(sum(data) / len(data))
-                self.stddev = float((sum((x - self.mean) ** 2 for x in data) /
-                                     (len(data) - 1)) ** 0.5)
+                # The standard deviation is the square root of the variance
+                variance = 0
+                for i in range(len(data)):
+                    # variance is calculated by taking the average of the
+                    # squares of the differences of each value from the mean.
+                    variance += (data[i] - self.mean) ** 2
+                self.stddev = (variance / len(data)) ** (1 / 2)
