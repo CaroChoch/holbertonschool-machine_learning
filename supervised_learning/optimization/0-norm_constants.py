a48be59479a92a97fef27daf1a3941bddda110ed
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-15 07:12:38 -0800

    function that calculates the normalization constants of a matrix

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Function that calculates the normalization (standardization) constants of a matrix """
+import numpy as np
+
+
+def normalization_constants(X):
+    """ 
+    Function that calculates the normalization (standardization) constants of a matrix 
+    Arguments:
+     - X is the numpy.ndarray of shape (m, nx) to normalize
+        - m is the number of data points
+        - nx is the number of features
+    """
+    return np.mean(X, axis=0), np.std(X, axis=0)
