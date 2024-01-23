65b30d77a34da6bf70d126c297f2b71e1f7678ad
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-23 08:57:51 -0800

    function that updates the weights and biases of a neural network using gradient descent with L2 regularization

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Function that updates the weights and biases of a neural
+network using gradient descent with L2 regularization
+"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """
+    Function that updates the weights and biases of a neural
+    network using gradient descent with L2 regularization
+    Arguments:
+     - Y is a one-hot numpy.ndarray of shape (classes, m) that contains
+        the correct labels for the data
+        * classes is the number of classes
+        * m is the number of data points
+     - weights is a dictionary of the weights and biases of the neural network
+     - cache is a dictionary of the outputs of each layer of the neural network
+     - alpha is the learning rate
+     - lambtha is the L2 regularization parameter
+     - L is the number of layers of the network
+    """
+    # Initialize the number of layers of the network
+    m = Y.shape[1]
+
+    # Copy the weights dictionary to new variables
+    weights_dictionary = weights.copy()
+
+    # Calculate the derivative of the loss function with respect to z
+    dz = cache['A' + str(L)] - Y
+
+    # Iterate through the layers backwards
+    for i in range(L, 0, -1):
+        # Calculate the L2 regularization term
+        l2_reg_term = (lambtha / m) * weights_dictionary['W' + str(i)]
+
+        # Calculate the derivative of the weights (with L2 regularization)
+        dw = (np.matmul(dz, cache['A' + str(i - 1)].T) / m) + l2_reg_term
+
+        # Calculate the derivative of the biases
+        db = np.sum(dz, axis=1, keepdims=True) / m
+
+        # Calculate the derivative of the previous layer's dz
+        dz = np.matmul(weights_dictionary['W' + str(i)].T, dz) * (
+            cache["A" + str(i - 1)] * (1 - cache["A" + str(i - 1)]))
+
+        # Update the weights and biases using gradient descent
+        weights['W' + str(i)] -= alpha * dw
+        weights['b' + str(i)] -= alpha * db
