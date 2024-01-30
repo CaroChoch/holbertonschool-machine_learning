540a04191de85f819dec8c1ccc80df0c187fe71a
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-30 04:51:52 -0800

    function that analyze validation data

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Function that trains a model using mini-batch gradient descent """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Function that trains a model using mini-batch gradient descent
+    Arguments:
+        - network: is the model to train
+        - data: is a numpy.ndarray of shape (m, nx) containing
+        the input data
+        - labels: is a one-hot numpy.ndarray of shape (m, classes)
+        containing the labels of data
+        - batch_size: is the size of the batch used for mini-batch
+        gradient descent
+        - epochs: is the number of passes through data for mini-batch
+        gradient descent
+        - validation_data : is the data to validate the model with
+        - verbose: is a boolean that determines if output should be
+        printed during training
+        - shuffle: is a boolean that determines whether to shuffle the
+        batches every epoch. Defaults to False.
+    Returns: the History object generated after training the model
+    """
+
+    history = network.fit(data,
+                          labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle)
+
+    return history
