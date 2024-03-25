b6a1ab74497826f08436ca8016d77d5bdf73cf2e
root <fleur-a-fleur59@hotmail.fr>
2024-03-25 14:08:36 +0000

    solve pycodestyle

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -6,21 +6,22 @@ import numpy as np
 
 def likelihood(x, n, P):
     """
-    Function that calculates the likelihood of obtaining this data given various
-    hypothetical probabilities of developing severe side effects
+    Function that calculates the likelihood of obtaining this data given
+    various hypothetical probabilities of developing severe side effects
     Arguments:
         * x is the number of patients that develop severe side effects
         * n is the total number of patients observed
-        * P is a 1D numpy.ndarray containing the various hypothetical probabilities of
-        developing severe side effects
-        
-    Returns: a 1D numpy.ndarray containing the likelihood of obtaining the data,
-        x and n, for each probability in P, respectively
-        """
+        * P is a 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
+
+    Returns: a 1D numpy.ndarray containing the likelihood of obtaining the
+        data, x and n, for each probability in P, respectively
+    """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
@@ -32,6 +33,7 @@ def likelihood(x, n, P):
     # P(x|n, P) = (n!/(x!(n-x)!)) * P^x * (1-P)^(n-x)
     fact = np.math.factorial
 
-    likelihood = fact(n) / (fact(x) * fact(n - x)) * (P ** x) * ((1 - P) ** (n - x))
+    likelihood = fact(n) / (fact(x) * fact(n - x)) * (P ** x) * (
+        (1 - P) ** (n - x))
 
     return likelihood
