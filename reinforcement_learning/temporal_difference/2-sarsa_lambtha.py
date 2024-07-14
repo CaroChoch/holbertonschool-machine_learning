d72f0dcac56656d039e272aada520a0ca7128c5d
root <fleur-a-fleur59@hotmail.fr>
2024-07-14 18:12:19 +0000

    Sarsa Lambda algorithm

---
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+""" Performs SARSA(λ) with eligibility trace """
+import numpy as np
+import gym
+
+
+def epsilon_greedy(state, Q, epsilon):
+    """
+    Epsilon-greedy policy to choose actions
+    Arguments:
+        - state: the current state
+        - Q: np.ndarray of shape(s, a) containing the Q table
+        - epsilon: the epsilon to use for the calculation
+    Returns: the action to take
+    """
+    # Generate a random number between 0 and 1
+    p = np.random.uniform()
+
+    if p > epsilon:
+        # Exploit : Choose the action with the highest Q value
+        action = np.argmax(Q[state])
+    else:
+        # Explore : Choose a random action
+        action = np.random.randint(Q.shape[1])
+
+    return action
+
+
+def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100,
+                  alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1,
+                  epsilon_decay=0.05):
+    """
+    Performs SARSA(λ) with eligibility trace
+    Arguments:
+        - env: the openAI environment instance
+        - Q: np.ndarray of shape(s, a) containing the Q table
+        - lambtha: the eligibility trace factor
+        - episodes: the total number of episodes to train over
+        - max_steps: the maximum number of steps per episode
+        - alpha: the learning rate
+        - gamma: the discount rate
+        - epsilon: the initial threshold for epsilon greedy
+        - min_epsilon: the minimum value that epsilon should decay to
+        - epsilon_decay: the decay rate for updating epsilon between episodes
+    Returns: Q, the updated Q table
+    """
+
+    # Initial value of epsilon for decay calculation
+    epsilon_init = epsilon
+
+    for ep in range(episodes):
+        # Reset environment to start a new episode
+        state = env.reset()
+        # Choose initial action using epsilon-greedy policy
+        action = epsilon_greedy(state, Q, epsilon)
+        # Initialize eligibility trace for Q table
+        eligibility_trace = np.zeros(Q.shape)
+
+        for step in range(max_steps):
+            # Take action in the environment and observe new state and reward
+            new_state, reward, done, _ = env.step(action)
+            # Choose new action using epsilon-greedy policy for new state
+            new_action = epsilon_greedy(new_state, Q, epsilon)
+
+            # Calculate TD error uning action-value form of Q
+            td_error = reward + gamma * Q[new_state, new_action] \
+                - Q[state, action]
+
+            # Update eligibility trace for current state and action
+            eligibility_trace[state, action] += 1
+            eligibility_trace *= gamma * lambtha
+
+            # Update Q table using SARSA(λ) update rule
+            Q += alpha * td_error * eligibility_trace
+
+            if done:
+                break  # Exit loop if episode is finished
+
+            # Update state and action for next iteration
+            state = new_state
+            action = new_action
+
+        # Decay epsilon according to exponential schedule
+        epsilon = min_epsilon + (epsilon_init - min_epsilon) * np.exp(
+            -epsilon_decay * ep)
+
+    # Return the updated Q table
+    return Q
