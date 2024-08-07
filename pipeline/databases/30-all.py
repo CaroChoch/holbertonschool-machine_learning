c4c3fc104df970cefa31f860bbec8d0c8e9aa3c9
root <fleur-a-fleur59@hotmail.fr>
2024-08-07 23:33:41 +0200

    Python function that lists all documents in a collection

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Python function that lists all documents in a collection """
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Function that lists all documents in a collection
+    Return : list of documents or empty list
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
