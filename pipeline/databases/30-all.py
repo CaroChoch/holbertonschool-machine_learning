c6dbcd8906a48f7a39f09a8d47a7e4b1a39dfc41
root <fleur-a-fleur59@hotmail.fr>
2024-12-06 16:38:15 +0100

    remove the condition

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -8,6 +8,4 @@ def list_all(mongo_collection):
     Function that lists all documents in a collection
     Return : list of documents or empty list
     """
-    if not mongo_collection:
-        return []
     return list(mongo_collection.find())
