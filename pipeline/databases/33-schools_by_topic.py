d668556c5f661c84dd0f4b58b1a2f04e48aac31d
root <fleur-a-fleur59@hotmail.fr>
2024-12-06 16:44:40 +0100

    remove the condition

--- pipeline/databases/33-schools_by_topic.py
+++ pipeline/databases/33-schools_by_topic.py
@@ -8,6 +8,4 @@ def schools_by_topic(mongo_collection, topic):
     Function that returns the list of school having a specific topic
     Return : list of schools or empty list
     """
-    if not mongo_collection or not topic:
-        return []
     return list(mongo_collection.find({"topics": topic}))
