d719f67f7d69c3787ad4e573c33ea3c662e8c908
root <fleur-a-fleur59@hotmail.fr>
2024-12-06 19:29:34 +0100

    fix the code by create a function

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-# Load the data from the  CSV file using the from_file function
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# Rename the column 'Timestamp' to 'Datetime'
-df = df.rename(columns={'Timestamp': 'Datetime'})
+def rename(df: pd.DataFrame):
+    """
+    Rename the column Timestamp to Datetime and convert the values in the
+    'Datetime' column to datetime objects
+    Arguments:
+        - df is the pd.DataFrame to modify
+    Returns:
+        The modified pd.DataFrame
+    """
 
-# Convert the timestamp values in 'Datetime' column to datetime objects
-df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+    # Rename the column 'Timestamp' to 'Datetime'
+    df = df.rename(columns={'Timestamp': 'Datetime'})
 
-# Display only the 'Datetime' and 'Close' columns
-df = df[['Datetime', 'Close']]
+    # Convert the timestamp values in 'Datetime' column to datetime objects
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
 
-# Print the last 5 rows of the DataFrame df
-print(df.tail())
+    # Display only the 'Datetime' and 'Close' columns
+    df = df[['Datetime', 'Close']]
+
+    return df
