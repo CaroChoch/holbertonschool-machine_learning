a5b05f883de3af301c1b5f7261813a1ba566be79
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 11:12:28 +0100

    add the function high

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+""" Sort the data in descending order of the 'High' order """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def high(df):
+    """
+    Sort the data in descending order of the 'High' order
+    df: pandas DataFrame
+    Return: new DataFrame
+    """
+    # Sort the data in descending order of the 'High' order
+    df = df.sort_values(by='High', ascending=False)
 
-# Sort the data in descending order of the 'High' order
-df = df.sort_values(by='High', ascending=False)
-
-print(df.head())
+    # Return the new DataFrame
+    return df
