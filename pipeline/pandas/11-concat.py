e76ff71bcbd7cbccbdf38217fa8751c065b1075a
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 12:08:05 +0100

    add the function concat

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
+""" Concatenate two pd.DataFrames """
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
+index = __import__('10-index').index
 
-df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-# Include all timestamps from bitstamp up to and including timestamp 1417411920
-df2 = df2.loc[:1417411920]
+def concat(df1, df2):
+    """
+    Concatenate two pd.DataFrames
+    df1: pd.DataFrame
+    df2: pd.DataFrame
+    Return: new DataFrame
+    """
+    # Include all timestamps from bitstamp up to and including timestamp 1417411920
+    df2 = df2.loc[:1417411920]
 
-# Set the index to the 'Timestamp' column
-df1 = df1.set_index('Timestamp')
-df2 = df2.set_index('Timestamp')
+    # Set the index to the 'Timestamp' column
+    df1 = df1.set_index('Timestamp')
+    df2 = df2.set_index('Timestamp')
 
-# Concatenate the pd.DataFrames
-df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    # Concatenate the pd.DataFrames
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
-print(df)
+    # Return the new DataFrame
+    return df
