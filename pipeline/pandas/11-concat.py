bfe46e7a3a0afb95d262a535f822cb39e58a85b7
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 12:31:19 +0100

    fix the dataframe

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Concatenate two pd.DataFrames """
 
+
 import pandas as pd
 index = __import__('10-index').index
 
@@ -12,12 +13,12 @@ def concat(df1, df2):
     df2: pd.DataFrame
     Return: new DataFrame
     """
-    # Include all timestamps from bitstamp up to and including timestamp 1417411920
-    df2 = df2.loc[:1417411920]
+    # Index the pd.DataFrames
+    df1 = index(df1)
+    df2 = index(df2)
 
-    # Set the index to the 'Timestamp' column
-    df1 = df1.set_index('Timestamp')
-    df2 = df2.set_index('Timestamp')
+    # Include all timestamps from bitstamp up to including timestamp 1417411920
+    df2 = df2.loc[:1417411920]
 
     # Concatenate the pd.DataFrames
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
