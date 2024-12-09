9c062406f89e8a85031eef8463a4d5df09e7bde5
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 11:42:12 +0100

    add the function prune

--- pipeline/pandas/8-prune.py
+++ pipeline/pandas/8-prune.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+""" Remove the entries in the pd.DataFrame where Close is NaN """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def prune(df):
+    """
+    Remove the entries in the pd.DataFrame where Close is NaN
+    Arguments:
+        - df is the pd.DataFrame to prune
+    Returns:
+        The pruned pd.DataFrame
+    """
 
-# Re;ove the entries in the pd.DataFrame where Close is NaN
-df = df.dropna(subset=['Close'])
+    # Remove the entries in the pd.DataFrame where Close is NaN
+    df = df.dropna(subset=['Close'])
 
-print(df.head())
+    # Return the pruned pd.DataFrame
+    return df
