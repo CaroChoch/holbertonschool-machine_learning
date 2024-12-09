52d7e0c7595313ec94820a132c928502fe952009
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 12:40:36 +0100

    add the function hierarchy

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
+""" Concatenate two dataframes with a MultiIndex """
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
+index = __import__('10-index').index
 
-df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-# Filter dataframes to include only rows with 'Timestamp' between 1417411980 and 1417417980
-df1 = df1.loc[
-    (df1['Timestamp'] >= 1417411980) & (df1['Timestamp'] <= 1417417980)]
-df2 = df2.loc[
-    (df2['Timestamp'] >= 1417411980) & (df2['Timestamp'] <= 1417417980)]
+def hierarchy(df1, df2):
+    """
+    takes two dataframes and concatenates them with a MultiIndex
+    Arguments:
+        - df1: pd.DataFrame
+        - df2: pd.DataFrame
+    Returns:
+        A new pd.DataFrame with a MultiIndex
+    """
 
-# Set 'Timestamp' column as the index for both dataframes
-df1 = df1.set_index('Timestamp')
-df2 = df2.set_index('Timestamp')
+    # Filter dataframes to include only rows with 'Timestamp' between
+    # 1417411980 and 1417417980
+    df1 = df1.loc[
+        (df1['Timestamp'] >= 1417411980) & (df1['Timestamp'] <= 1417417980)]
+    df2 = df2.loc[
+        (df2['Timestamp'] >= 1417411980) & (df2['Timestamp'] <= 1417417980)]
 
-# Concatenate the two dataframes along the indexm creating a MultiIndex with keys 'bitstamp' and 'coinbase'
-df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    # Index the pd.DataFrames
+    df1 = index(df1)
+    df2 = index(df2)
 
-# Swap the levels of the MultiIndex and sort the index to display the rows in chronological order
-df = df.swaplevel(0, 1, axis=0).sort_index()
+    # Concatenate the two dataframes along the indexm creating a MultiIndex
+    # with keys 'bitstamp' and 'coinbase'
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
-print(df)
+    # Swap the levels of the MultiIndex and sort the index to display the rows
+    # in chronological order
+    df = df.swaplevel(0, 1, axis=0).sort_index()
+
+    # Return the new DataFrame
+    return df
