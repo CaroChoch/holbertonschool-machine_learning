d473446de20de9c84a201c18f668fb31c449e466
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 12:45:35 +0100

    fix pep8

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-""" Calculate descriptive statistics for all columns in pd.DataFrame except Timestamp """
+"""
+Calculate descriptive statistics for all columns in pd.DataFrame except
+Timestamp
+"""
 
 
 def analyze(df):
     """
-    Calculate descriptive statistics for all columns in pd.DataFrame except Timestamp
+    Calculate descriptive statistics for all columns in pd.DataFrame except
+    Timestamp
     Arguments:
         - df is the pd.DataFrame to analyze
     Returns:
-        The descriptive statistics for all columns in the pd.DataFrame except Timestamp
+        The descriptive statistics for all columns in the pd.DataFrame except
+        Timestamp
     """
-    # calculate descriptive statistics for all columns in pd.DataFrame except Timestamp
+    # calculate descriptive statistics for all columns in pd.DataFrame except
+    # Timestamp
     stats = df.drop(columns=['Timestamp']).describe()
 
     # Return the descriptive statistics
