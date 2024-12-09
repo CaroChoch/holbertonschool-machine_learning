002249436bc9e724a2ebb4eb83e8ae7ef597e1a5
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 11:04:18 +0100

    add a function

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+""" Function that slices a DataFrame """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-# Load the data from the CSV file using the from_file function
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def slice(df):
+    """
+    Function that flips the switch on a DataFrame
+    Return : the new DataFrame
+    """
+    # Select only the 'High', 'Low', 'Close', and 'Volume_(BTC)' columns
+    # Use iloc with step 60 to select every 60th row, effectively resampling
+    # the data to hourly intervals
+    df = df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
 
-# Select only the 'High', 'Low', 'Close', and 'Volume_(BTC)' columns
-# Use iloc with step 60 to select every 60th row, effectively resampling
-# the data to hourly intervals
-df = df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
-
-# Print the last 5 rows of the DataFrame
-print(df.tail())
+    # Return the new DataFrame
+    return df
