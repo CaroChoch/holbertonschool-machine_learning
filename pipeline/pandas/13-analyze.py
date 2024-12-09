b8b44095c4a0d871a81c023b339921c77c95dd2b
root <fleur-a-fleur59@hotmail.fr>
2024-12-09 12:42:52 +0100

    add function analyse

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
+""" Calculate descriptive statistics for all columns in pd.DataFrame except Timestamp """
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# calculate descriptive statistics for all columns in pd.DataFrame except Timestamp
-stats = df.drop(columns=['Timestamp']).describe()
+def analyze(df):
+    """
+    Calculate descriptive statistics for all columns in pd.DataFrame except Timestamp
+    Arguments:
+        - df is the pd.DataFrame to analyze
+    Returns:
+        The descriptive statistics for all columns in the pd.DataFrame except Timestamp
+    """
+    # calculate descriptive statistics for all columns in pd.DataFrame except Timestamp
+    stats = df.drop(columns=['Timestamp']).describe()
 
-print(stats)
+    # Return the descriptive statistics
+    return stats
+
