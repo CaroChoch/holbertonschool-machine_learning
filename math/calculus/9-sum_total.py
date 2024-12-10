4b7b42f5c0234a78bf1bc6a54514634b760a8f4b
root <fleur-a-fleur59@hotmail.fr>
2024-12-10 10:46:18 +0100

    pycodestyle fixing

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -15,7 +15,7 @@ def summation_i_squared(n):
     """
 
     # Check if n is a valid number
-    if not type(n) == int or n < 1:
+    if not isinstance(n, int) or n < 1:
         return None
 
     # Calculate the sum using the closed-form formula:
