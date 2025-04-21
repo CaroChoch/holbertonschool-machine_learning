0ce783a0f57821ceb5e71e301091cdd880545b43
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 15:25:50 +0200

    Displays a map of altitude dispersion on a mountain

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
+"""Displays a map of altitude dispersion on a mountain"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
 
-x = np.random.randn(2000) * 10
-y = np.random.randn(2000) * 10
-z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+def gradient():
+    """
+    Displays a map of altitude dispersion on a mountain
+    """
+    np.random.seed(5)
 
-plt.xlabel('x coordinate (m)')
-plt.ylabel('y coordinate (m)')
-plt.title('Mountain Elevation')
-plt.colorbar(plt.scatter(x, y, c=z), label="elevation (m)")
-plt.show()
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.title('Mountain Elevation')
+    plt.colorbar(plt.scatter(x, y, c=z), label="elevation (m)")
+    plt.show()
