1856ded630781d2168b008f7550f9c950f0a7257
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 16:19:55 +0200

    update RMSProp optimizer for TF@ compatibility

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -3,29 +3,25 @@
 Function that creates the training operation for a neural
 network in tensorflow using the RMSProp optimization algorithm
 """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
+def create_RMSProp_op(alpha, beta2, epsilon):
     """
     Function that creates the training operation for a neural
     network in tensorflow using the RMSProp optimization algorithm
     Arguments:
-     - loss is the loss of the network
      - alpha is the learning rate
      - beta2 is the RMSProp weight
      - epsilon is a small number to avoid division by zero
     Returns:
-     The RMSProp optimization operation
+     The RMSProp optimizer
     """
 
     # Define the optimizer with RMSProp
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
-                                          decay=beta2,
+    optimizer = tf.keras.optimizers.RMSProp(learning_rate=alpha,
+                                          rho=beta2,
                                           epsilon=epsilon)
 
-    # Create the RMSProp optimization operation
-    RMSProp_op = optimizer.minimize(loss)
-
     # Return the RMSProp optimization operation
-    return RMSProp_op
+    return optimizers
