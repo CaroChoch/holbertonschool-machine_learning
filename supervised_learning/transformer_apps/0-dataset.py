9ca73c0bc8e7cc1d9b077b387988db06a38d9def
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 17:52:49 +0200

    Load pretrained Hugging face tokenizers for Portuguese and english

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -2,48 +2,34 @@
 """ Dataset class """
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
+import transformers
 
 
 class Dataset:
     """ Dataset class """
+
     def __init__(self):
         """ Class constructor """
-        # Load the dataset with metadata and as supervised
-        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
-                                       with_info=True,
-                                       as_supervised=True)
+        examples, metadata = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            with_info=True,
+            as_supervised=True
+        )
         self.metadata = metadata
-        self.data_train = examples['train']  # training dataset
-        self.data_valid = examples['validation']  # Validation dataset
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)  # Create sub-word tokenizers
+            self.data_train
+        )
 
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for dataset
-        Arguments:
-            - data is a tf.data.Dataset whose examples are formatted as a
-              tuple (pt, en)
-                - pt is the tf.Tensor containing the Portuguese sentence
-                - en is the tf.Tensor containing the corresponding English
-                  sentence
-        Returns:
-            tokenizer_pt, tokenizer_en
-                - tokenizer_pt is the Portuguese tokenizer
-                - tokenizer_en is the English tokenizer
+        Charge les tokenizers pré-entraînés
         """
-        # Create Portuguese tokenizer from the corpus
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.\
-            build_from_corpus(
-                (pt.numpy() for pt, en in data),
-                target_vocab_size=2**15
-            )
-
-        # Create English tokenizer from the corpus
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.\
-            build_from_corpus(
-                (en.numpy() for pt, en in data),
-                target_vocab_size=2**15
-            )
-
+        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
+            'bert-base-uncased'
+        )
         return tokenizer_pt, tokenizer_en
