c8834bcd7493bd2d91b133423228cba7b8d3dfb5
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 15:12:32 +0200

    Encapsulate code with a frequency function

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
+""" Display a histogram of student grades for Project A"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-student_grades = np.random.normal(68, 15, 50)
 
-plt.xlabel('Grades')
-plt.ylabel('Number of Students')
-plt.title('Project A')
-plt.xlim(0, 100)
-plt.ylim(0, 30)
+def frequency():
+    """
+    Display a histogram of student grades for Project A
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
-plt.xticks(np.arange(0, 101, 10))
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
-plt.show()
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.xticks(np.arange(0, 101, 10))
+
+    plt.show()
