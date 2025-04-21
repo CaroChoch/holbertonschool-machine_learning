8da9cfe0de05e5cfdfab4e2182f19041cfb0ed71
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 16:33:12 +0200

    Use tf.keras.optimizers.Adam for TF2 compatibility

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -3,31 +3,27 @@
 Function that creates the training operation for a neural network in tensorflow
 using the Adam optimization algorithm
 """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
     Function that creates the training operation for a neural network in
     tensorflow using the Adam optimization algorithm
     Arguments:
-     - loss is the loss of the network
      - alpha is the learning rate
      - beta1 is the weight used for the first moment
      - beta2 is the weight used for the second moment
      - epsilon is a small number to avoid division by zero
     Returns:
-     The Adam optimization operation
+     The Adam optimizer
     """
 
     # Define the optimizer with Adam
-    optimizer = tf.train.AdamOptimizer(learning_rate=alpha,
-                                       beta1=beta1,
-                                       beta2=beta2,
-                                       epsilon=epsilon)
-
-    # Create the Adam optimization operation
-    Adam_op = optimizer.minimize(loss)
+    optimizer = tf.keras.optimizer.Adam(learning_rate=alpha,
+                                        beta1=beta1,
+                                        beta2=beta2,
+                                        epsilon=epsilon)
 
     # Return the Adam optimization operation
-    return Adam_op
+    return optimizer
