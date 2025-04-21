b827eb773201510d8de03fb7b8c650f03078a2a7
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 14:34:02 +0200

    correct plot order to match reference

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -15,8 +15,8 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
+    plt.scatter(x, y, color='magenta')
+    plt.title('Men\'s Height vs Weight')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
-    plt.title('Men\'s Height vs Weight')
-    plt.scatter(x, y, color='magenta', s=8)
     plt.show()
