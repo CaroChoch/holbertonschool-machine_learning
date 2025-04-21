7a3734174346520233515296f060b16ed5b7f154
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 14:17:16 +0200

    add line function

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,9 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
+def line():
+  """
+  Draw a red cubic curve for x
+  ranging from 0 to 10
+  """
+  y = np.arange(0, 11) ** 3
 
-plt.figure(figsize=(6.4, 4.8))
-plt.plot(y, color='red')
-plt.xlim(0, 10)
-plt.show()
+  plt.figure(figsize=(6.4, 4.8))
+  plt.plot(y, color='red')
+  plt.xlim(0, 10)
+  plt.show()
