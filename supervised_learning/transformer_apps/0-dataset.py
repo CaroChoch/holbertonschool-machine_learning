99414a5dd4e9ad9f21a78c1fc086b473d60084f6
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-04-21 18:18:16 +0200

    Tokenizers to train data

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -23,12 +23,23 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for dataset using pretrained models
+        Creates sub-word tokenizers for dataset using pretrained
+        models adapted to our data
         """
-        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased'
-        )
-        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
-            'bert-base-uncased'
-        )
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in tfds.as_numpy(data):
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
+
+        pretrained_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased', use_fast=True)
+        pretrained_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased', use_fast=True)
+
+        tokenizer_pt = pretrained_pt.train_new_from_iterator(
+            pt_sentences, vocab_size=2**13)
+        tokenizer_en = pretrained_en.train_new_from_iterator(
+            en_sentences, vocab_size=2**13)
+
         return tokenizer_pt, tokenizer_en
