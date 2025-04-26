166db5ef711c9c3239e07b56186df0270f50c781
Caroline Chochoy <carolinechochoy@Mac.hbtn.fr>
2025-04-26 16:01:23 +0200

    change the import

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -3,7 +3,7 @@
 Function that builds the inception network as described in Going
 Deeper with Convolutions (2014)
 """
-import tensorflow.keras as K
+from tensorflow import keras as K
 inception_block = __import__('0-inception_block').inception_block
 
 
