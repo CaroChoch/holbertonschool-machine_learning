7d2b4fed09b667f9edb735726b207a56ff70b538
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-05-09 23:59:46 +0200

    Update 3-dataset.py

--- supervised_learning/transformer_apps/3-dataset.py
+++ supervised_learning/transformer_apps/3-dataset.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-"""Set up the data pipeline"""
+"""
+Prepare, encode, and set up a data pipeline for the TED HRLR
+Portuguese-English translation dataset using pre-trained tokenizers.
+"""
+
+import tensorflow as tf
 import tensorflow_datasets as tfds
 import transformers
-import tensorflow as tf
 
 
 class Dataset:
-    """Dataset class"""
+    """Load and preprocess the TED HRLR translation dataset."""
 
     def __init__(self, batch_size, max_len):
-        """
-        Class constructor:
-        - batch_size: batch size for training/validation
-        - max_len: maximum number of tokens allowed per sentence
-        """
-        self.batch_size = batch_size
-        self.max_len = max_len
-
+        """Initialize dataset, tokenizers, and data pipeline."""
         self.data_train = tfds.load(
-            'ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+            'ted_hrlr_translate/pt_to_en',
+            split='train', as_supervised=True)
         self.data_valid = tfds.load(
-            'ted_hrlr_translate/pt_to_en', split='validation',
-            as_supervised=True)
+            'ted_hrlr_translate/pt_to_en',
+            split='validation', as_supervised=True)
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
@@ -30,69 +28,69 @@ class Dataset:
             self.tf_encode, num_parallel_calls=tf.data.AUTOTUNE)
         self.data_train = self.data_train.filter(
             lambda pt, en: tf.logical_and(
-                tf.size(pt) <= max_len,
-                tf.size(en) <= max_len))
+                tf.size(pt) <= max_len, tf.size(en) <= max_len))
         self.data_train = self.data_train.cache()
-        self.data_train = self.data_train.shuffle(20000)
+        self.data_train = self.data_train.shuffle(buffer_size=20000)
         self.data_train = self.data_train.padded_batch(
-            self.batch_size, padded_shapes=([None], [None]))
+            batch_size, padded_shapes=([None], [None]))
         self.data_train = self.data_train.prefetch(
-            tf.data.experimental.AUTOTUNE)
+            buffer_size=tf.data.AUTOTUNE)
 
         self.data_valid = self.data_valid.map(
             self.tf_encode, num_parallel_calls=tf.data.AUTOTUNE)
         self.data_valid = self.data_valid.filter(
             lambda pt, en: tf.logical_and(
-                tf.size(pt) <= max_len,
-                tf.size(en) <= max_len))
+                tf.size(pt) <= max_len, tf.size(en) <= max_len))
         self.data_valid = self.data_valid.padded_batch(
-            self.batch_size, padded_shapes=([None], [None]))
-        self.data_valid = self.data_valid.prefetch(
-            tf.data.experimental.AUTOTUNE)
+            batch_size, padded_shapes=([None], [None]))
 
     def tokenize_dataset(self, data):
-        """
-        Create SubwordTextEncoder tokenizers using first 10,000 examples
-        to speed up tokenizer construction.
-        """
+        """Create and train tokenizers from the dataset."""
         pt_sentences = []
         en_sentences = []
-        for pt, en in data.take(10000).as_numpy_iterator():
+        for pt, en in data.as_numpy_iterator():
             pt_sentences.append(pt.decode('utf-8'))
             en_sentences.append(en.decode('utf-8'))
 
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.\
-            build_from_corpus(pt_sentences, target_vocab_size=2**15)
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.\
-            build_from_corpus(en_sentences, target_vocab_size=2**15)
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased',
+            use_fast=True, clean_up_tokenization_spaces=True)
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased', use_fast=True,
+            clean_up_tokenization_spaces=True)
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            pt_sentences, vocab_size=2**13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            en_sentences, vocab_size=2**13)
+
         return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
-        """
-        Encode a pair of sentences into token IDs with start/end tokens.
-        """
-        pt_start = self.tokenizer_pt.vocab_size
-        pt_end = pt_start + 1
-        en_start = self.tokenizer_en.vocab_size
-        en_end = en_start + 1
+        """Encode sentences and add start/end tokens."""
+        pt_sentence = pt.numpy().decode('utf-8')
+        en_sentence = en.numpy().decode('utf-8')
 
-        pt_text = pt.numpy().decode('utf-8')
-        en_text = en.numpy().decode('utf-8')
+        pt_tokens = self.tokenizer_pt.encode(
+            pt_sentence, add_special_tokens=False)
+        en_tokens = self.tokenizer_en.encode(
+            en_sentence, add_special_tokens=False)
 
-        pt_tokens = [pt_start] + self.tokenizer_pt.encode(pt_text) + [pt_end]
-        en_tokens = [en_start] + self.tokenizer_en.encode(en_text) + [en_end]
+        pt_tokens = [self.tokenizer_pt.vocab_size] + pt_tokens + \
+            [self.tokenizer_pt.vocab_size + 1]
+        en_tokens = [self.tokenizer_en.vocab_size] + en_tokens + \
+            [self.tokenizer_en.vocab_size + 1]
+
         return pt_tokens, en_tokens
 
     def tf_encode(self, pt, en):
-        """
-        TensorFlow wrapper around the encode method.
-        Returns two tf.int64 tensors with shape [None].
-        """
+        """TensorFlow wrapper for encode method."""
         pt_tokens, en_tokens = tf.py_function(
             func=self.encode,
             inp=[pt, en],
-            Tout=[tf.int64, tf.int64]
-        )
+            Tout=[tf.int64, tf.int64])
+
         pt_tokens.set_shape([None])
         en_tokens.set_shape([None])
+
         return pt_tokens, en_tokens
