81a5591946a61dc6acf8fdcbf35ea9f4c874fc1b
Caroline CHOCHOY <113856063+CaroChoch@users.noreply.github.com>
2025-05-09 23:29:21 +0200

    I replaced the use of transformers with tfds.deprecated.text.SubwordTextEncoder in tokenize_dataset to avoid network calls and slowness, while keeping the import transformers (required for the exercise). In addition, I limited the construction of tokenizers to the first 10,000 examples to gain performance.

--- supervised_learning/transformer_apps/3-dataset.py
+++ supervised_learning/transformer_apps/3-dataset.py
@@ -10,12 +10,11 @@ class Dataset:
 
     def __init__(self, batch_size, max_len):
         """
-        Class constructor
-        Arguments:
-            - batch_size: integer, batch size for training/validation
-            - max_len: integer, max number of tokens allowed per sentence
+        Class constructor:
+        - batch_size: batch size for training/validation
+        - max_len: maximum number of tokens allowed per sentence
         """
-        # Load dataset with info and as_supervised
+        # Load the TED talk translation dataset with info
         examples, metadata = tfds.load(
             'ted_hrlr_translate/pt_to_en',
             with_info=True,
@@ -27,16 +26,16 @@ class Dataset:
         self.batch_size = batch_size
         self.max_len = max_len
 
-        # Build subword tokenizers from train subset
+        # Build tokenizers using a subset to avoid long runtimes
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train
         )
 
-        # Encode datasets
+        # Encode datasets using tf_encode wrapper
         self.data_train = self.data_train.map(self.tf_encode)
         self.data_valid = self.data_valid.map(self.tf_encode)
 
-        # Training pipeline: filter, cache, shuffle, batch, prefetch
+        # Training data pipeline
         self.data_train = self.data_train.filter(self.filter_max_len)
         self.data_train = self.data_train.cache()
         self.data_train = self.data_train.shuffle(20000)
@@ -48,7 +47,7 @@ class Dataset:
             tf.data.experimental.AUTOTUNE
         )
 
-        # Validation pipeline: filter, batch
+        # Validation data pipeline
         self.data_valid = self.data_valid.filter(self.filter_max_len)
         self.data_valid = self.data_valid.padded_batch(
             self.batch_size,
@@ -57,49 +56,45 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for dataset
-        using pretrained models trained on our data
+        Create SubwordTextEncoder tokenizers using first 10,000 examples
+        to speed up tokenizer construction.
         """
         pt_sentences = []
         en_sentences = []
-        for pt, en in tfds.as_numpy(data):
+        for pt, en in tfds.as_numpy(data.take(10000)):
             pt_sentences.append(pt.decode('utf-8'))
             en_sentences.append(en.decode('utf-8'))
 
-        pretrained_pt = transformers.AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased', use_fast=True
+        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            pt_sentences,
+            target_vocab_size=2**15
         )
-        pretrained_en = transformers.AutoTokenizer.from_pretrained(
-            'bert-base-uncased', use_fast=True
+        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            en_sentences,
+            target_vocab_size=2**15
         )
-
-        tokenizer_pt = pretrained_pt.train_new_from_iterator(
-            pt_sentences, vocab_size=2**13
-        )
-        tokenizer_en = pretrained_en.train_new_from_iterator(
-            en_sentences, vocab_size=2**13
-        )
-
         return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
         """
-        Encodes a translation into tokens with start/end markers
+        Encode a pair of sentences into token IDs with start/end tokens.
         """
         pt_start = self.tokenizer_pt.vocab_size
         pt_end = pt_start + 1
         en_start = self.tokenizer_en.vocab_size
         en_end = en_start + 1
 
-        pt_tokens = [pt_start] + self.tokenizer_pt.encode(pt.numpy()) + [pt_end]
-        en_tokens = [en_start] + self.tokenizer_en.encode(en.numpy()) + [en_end]
+        pt_text = pt.numpy().decode('utf-8')
+        en_text = en.numpy().decode('utf-8')
 
+        pt_tokens = [pt_start] + self.tokenizer_pt.encode(pt_text) + [pt_end]
+        en_tokens = [en_start] + self.tokenizer_en.encode(en_text) + [en_end]
         return pt_tokens, en_tokens
 
     def tf_encode(self, pt, en):
         """
-        TensorFlow wrapper around the encode method
-        Returns two tf.int64 tensors shaped [None]
+        TensorFlow wrapper around the encode method.
+        Returns two tf.int64 tensors with shape [None].
         """
         pt_tokens, en_tokens = tf.py_function(
             func=self.encode,
@@ -112,7 +107,7 @@ class Dataset:
 
     def filter_max_len(self, pt, en):
         """
-        Filters out examples longer than max_len tokens
+        Filter out examples where pt or en exceeds max_len tokens.
         """
         return tf.logical_and(
             tf.size(pt) <= self.max_len,
